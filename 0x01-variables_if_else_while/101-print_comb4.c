94c4b7bb0b8e42a09ec30debd64c35b4388217c5
AbdelwadoudMakh55 <Abdelwadoud~@gmail.com>
2023-06-16 16:37:48 -0700

    Commit of task 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	while (i < 8)
+	{
+		j = i + 1;
+		while (j < 9)
+		{
+			k = j + 1;
+			while (k < 10)
+			{
+				putchar((char)n + i);
+				putchar((char)n + j);
+				putchar((char)n + k);
+				k++;
+				if (n + i != 55)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			k = 0;
+			j++;
+		}
+		j=0;
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
