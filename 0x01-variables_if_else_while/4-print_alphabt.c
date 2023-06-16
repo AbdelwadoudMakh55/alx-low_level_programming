12576def2b81a59bb3e649b01ae798ceb4562953
AbdelwadoudMakh55 <Abdelwadoud~@gmail.com>
2023-06-16 11:02:34 -0700

    Commit of task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	int n = 97;
+	
+	while ((i < 26) && (i != 101) && (i != 113))
+	{
+		putchar((char)n);
+		n++;
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
