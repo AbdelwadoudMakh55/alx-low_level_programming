76afa1754f02abb54a4721c3551e05a9589292f8
AbdelwadoudMakh55 <Abdelwadoud~@gmail.com>
2023-06-16 10:54:49 -0700

    Commit of task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	int i = 0;
+	
+	int n = 97;
+	
+	int m = 65;
+	
+	while (i < 26)
+	{
+		putchar((char)n);
+		n++;
+		i++;
+	}
+	i = 0;
+	while (i < 26)
+	{
+		putchar((char)m);
+		m++;
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
