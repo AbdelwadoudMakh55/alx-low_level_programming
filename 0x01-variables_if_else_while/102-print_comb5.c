ba5bf03702fa486d6a5887fedb4f3d6007f17b13
AbdelwadoudMakh55 <Abdelwadoud~@gmail.com>
2023-06-16 17:19:32 -0700

    Commit of task 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int i = 0;
+        int j = 0; 
+        int k = 0;
+        int l = 0;
+        int n = 48;
+
+        while (i < 10)
+        {
+                //j = i + 1;
+                while (j < 9)
+                {
+                	k = i;
+                	while (k < 10)
+                	{
+                		l = j + 1;
+                		while (l < 10)
+                		{
+                        	putchar((char)n + i);
+                        	putchar((char)n + j);
+                        	putchar(' ');
+                        	putchar((char)n + k);
+                        	putchar((char)n + l);
+                        	l++;
+                        	if (!(i == 9 && j == 8 && k == 9 && l == 9))
+                        	{
+                            	putchar(',');
+                            	putchar(' ');
+                        	}
+                        }
+                    	l = 0;
+                    	k++;
+                	}
+                	k = 0;
+                	j++;
+            	}
+            	j = 0 ;
+		i++;
+	}
+		putchar('\n');
+		return (0);
+}
