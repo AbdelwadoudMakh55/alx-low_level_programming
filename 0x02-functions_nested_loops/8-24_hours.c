e7855b84451832df9042d35aa582d9ef2069cc9d
AbdelwadoudMakh55 <Abdelwadoud~@gmail.com>
2023-06-20 09:49:16 -0700

    Commit of task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * jack_bauer - Entry point
+ *
+ * Return: Always void (Success)
+ */
+void jack_bauer(void)
+{
+	int i = 0;
+	int j = 0;
+	int n = 48;
+
+	while (i < 24)
+	{
+		while (j < 60)
+		{
+			_putchar((char)n + i / 10);
+			_putchar((char)n + i % 10);
+			_putchar(':');
+			_putchar((char)n + j / 10);
+			_putchar((char)n + j % 10);
+			j++;
+		}
+		i++;
+	}
+}
