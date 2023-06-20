18d550f04c2126bbc3f173f935d17f64838d9dc1
AbdelwadoudMakh55 <Abdelwadoud~@gmail.com>
2023-06-20 05:08:57 -0700

    Commit of task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i = 0;
+	int n = 97;
+
+	while (i < 26)
+	{
+		_putchar((char)n + i);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
