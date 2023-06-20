a83dbc24b687cb3f5ee81b4170c7e1220e1d02a9
AbdelwadoudMakh55 <Abdelwadoud~@gmail.com>
2023-06-20 14:54:42 -0700

    Commit of task 100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,52 @@
+#include "main.h"
+/**
+ * print_times_table - Entry point
+ * @n : integer
+ * Return: Always void (Success)
+ */
+void print_times_table(int n)
+{
+	int i;
+	int j;
+	int track_last_num = 1;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			if (48 + i * j <= 57 && 48 + i * j >= 48)
+			{
+				if (!track_last_num)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else
+					track_last_num = 0;
+				_putchar((char)48 + i * j);
+			}
+			else
+			{
+				if (!track_last_num)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else
+					track_last_num = 0;
+				_putchar((char)48 + i * j / 10);
+				_putchar((char)48 + i * j % 10);
+			}
+		}
+		track_last_num = 0;
+		j = 0;
+		_putchar('\n');
+	}
+}
+
+
+				
+
+		
