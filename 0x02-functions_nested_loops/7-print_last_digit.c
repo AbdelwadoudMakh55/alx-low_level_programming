3e484089fc3fa874ccb47908ecce9827c9271353
AbdelwadoudMakh55 <Abdelwadoud~@gmail.com>
2023-06-20 07:13:45 -0700

    Commit of task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,18 +4,18 @@
  * @n : int
  * Return: last digit of n (Success)
  */
-
 int print_last_digit(int n)
 {
-
 	int last_digit = 0;
 	int abs = 0;
 
-        if (n >= 0)
-                abs = n;
+	if (n >= 0)
+		abs = n;
         else
-                abs = n * -1;
+		abs = n * -1;
 	last_digit = abs % 10;
+	if (last_digit < 0)
+		last_digit = -1 * last_digit;
 	_putchar((char)last_digit + 48);
 	return (last_digit);
 }
