95e98912e260e69a1840b832647e6fbdd0164e54
AbdelwadoudMakh55 <Abdelwadoud~@gmail.com>
2023-06-20 06:14:03 -0700

    Commit of task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,27 +1,26 @@
 #include "main.h"
 /**
  * print_sign - Entry point
- * @c : int
+ * @n : int
  * Return: 1 or 0 (Success)
  */
 int print_sign(int n)
 {
+	int r_value = 0;
+
 	if (n == 0)
 	{
 		_putchar('0');
-		_putchar(',');
-		return (0);
 	}
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(',');
-		return (1);
+		r_value = 1;
 	}
 	if (n < 0)
 	{
 		_putchar('-');
-		_putchar(',');
-		return (-1);
+		r_value = -1;
 	}
+	return (r_value);
 }
