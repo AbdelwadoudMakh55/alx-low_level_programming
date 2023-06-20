32cb4b033fec0cb713a7d7a6091190951862ca9a
AbdelwadoudMakh55 <Abdelwadoud~@gmail.com>
2023-06-20 05:28:03 -0700

    Commit of task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * islower - Entry point
- *
+ * c : int
  * Return: Always void (Success)
  */
 int _islower(int c)
 {
 	if (c <= 122 && c >= 97)
-		return 1;
+		return (1);
 	else
-		return 0;
+		return (0);
 }
