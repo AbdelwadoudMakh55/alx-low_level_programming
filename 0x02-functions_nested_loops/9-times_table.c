7154207394d92e04d30594c25c0ca1a365bc2e8f
AbdelwadoudMakh55 <Abdelwadoud~@gmail.com>
2023-06-20 11:02:56 -0700

    Commit of task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,20 +1,23 @@
 #include "main.h"
-/* * times_table - Entry point  * Return: Always void (Success) */
-void times_table(void){
+/* * times_table - Entry point
+   * Return: Always void (Success) */
+void times_table(void)
+{
 	int i = 0;
 	int j = 0;
-	int n = 48;
 	int track_last_num = 1;
 
 	while (i < 10)
 	{
 		while (j < 10)
 		{
-			if (i * j + n <= 57 && i * j + n >= 48)
+			if (i * j + 48 <= 57 && i * j + 48 >= 48)
 			{
 				if (!track_last_num)
 				{
-					_putchar(',');_putchar(' ');_putchar(' ');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
 				}
 				else
 					track_last_num = 0;
@@ -24,11 +27,13 @@ void times_table(void){
 			{
 				if (!track_last_num)
 				{
-					_putchar(',');_putchar(' ');
+					_putchar(',');
+					_putchar(' ');
 				}
 				else
 					track_last_num = 0;
-				_putchar((char)n + i * j / 10);_putchar((char)n + i * j % 10);
+				_putchar((char)48 + i * j / 10);
+				_putchar((char)48 + i * j % 10);
 			}
 			j++;
 		}
