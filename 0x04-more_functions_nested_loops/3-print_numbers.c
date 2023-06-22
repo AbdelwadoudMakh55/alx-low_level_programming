0d3b382e7a90f7844cb2802a3ecc03f455921519
AbdelwadoudMakh55 <Abdelwadoud~@gmail.com>
2023-06-22 13:29:33 -0700

    Commit of task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - check the code
+ *
+ * Return: void.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		_putchar((char)48 + i);
+}
