d7334195ad5bc661e87915557d903c3a7cbf07af
AbdelwadoudMakh55 <Abdelwadoud~@gmail.com>
2023-06-22 12:58:46 -0700

    Commit of task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - Entry point
+ * @c : integer
+ * Return: Always int (Success)
+ */
+int _isupper(int c)
+{
+	if (int(c) <= 90 && int(c) >= 65)
+		return (1);
+	else 
+		return (0);
+}
