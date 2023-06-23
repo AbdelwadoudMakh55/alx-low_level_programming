af87c1ad4cb9901f835ea31249f50dd2ba576a26
AbdelwadoudMakh55 <Abdelwadoud~@gmail.com>
2023-06-23 05:51:21 -0700

    Commit of task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,13 +12,15 @@ int main(void)
 	for (i = 1; i < 101; i++)
 	{
 		if (i % 3 == 0 && !(i % 5 == 0))
-			printf("Fizz ");
+			printf("Fizz");
 		if (i % 5 == 0 && !(i % 3 == 0))
-			printf("Buzz ");
+			printf("Buzz");
 		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		if (!(i % 3 == 0) && !(i % 5 == 0))
-			printf("%d ", i);
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
