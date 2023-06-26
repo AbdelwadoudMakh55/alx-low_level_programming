35212af7cf3e3b50a05d48b7d62184f030e46afd
AbdelwadoudMakh55 <Abdelwadoud315@gmail.com>
2023-06-26 11:54:50 +0000

    Commit of task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *  reset_to_98 - check the code 
- *  @*n : pointer to integer.
- *  Return: Always void .    
+ *  reset_to_98 - check the code
+ *  @n : pointer to integer.
+ *  Return: Always void .
  */
 void reset_to_98(int *n)
 {
