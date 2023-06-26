a2899552a914e56996a321c8d444779ab967e0f8
AbdelwadoudMakh55 <Abdelwadoud315@gmail.com>
2023-06-26 14:30:36 +0000

    Commit of task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,22 @@
 #include "main.h"
 /**
+ * _strlen - return the length of a string.
+ * @s : pointer to a string.
+ * Return: len; length of string str.
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	int len = 0;
+
+	while (*(s + i) != '\0')
+	{
+		len += 1;
+		i++;
+	}
+	return (len);
+}
+/**
  * puts_half - print the 2nd half of a string.
  * @str : pointer to a string.
  * Return: void.
