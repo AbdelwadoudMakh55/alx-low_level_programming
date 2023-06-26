5b960afa7eb172818f0ea01cb5728504b30d399f
AbdelwadoudMakh55 <Abdelwadoud315@gmail.com>
2023-06-26 16:27:39 +0000

    Commit of task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,8 +8,13 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
+	int n;
 
 	for (i = 0; *(src + i) != '\0'; i++)
+	{
 		*(dest + i) = *(src + i);
+		n = i;
+	}
+	*(dest + n + 1) = '\0';
 	return (dest);
 }
