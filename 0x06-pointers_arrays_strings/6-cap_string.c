28d86ad3b8faa0e75e479aba4b8b0ccfc49309c9
AbdelwadoudMakh55 <Abdelwadoud315@gmail.com>
2023-06-29 19:12:16 +0000

    Commit of task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,8 +17,6 @@ char *cap_string(char *str)
 		|| *(str + i) == '\t')
 		&& (*(str + i + 1) <= 122 && (int)*(str + i + 1) >= 97))
 			*(str + i + 1) -= 32;
-		if (*(str + i) == '\t')
-			*(str + i) = ' ';
 	}
 	return (str);
 }
