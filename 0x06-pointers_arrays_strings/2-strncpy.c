90efbacdff056be4df3ecdff4c1387f1ebc1612c
AbdelwadoudMakh55 <Abdelwadoud315@gmail.com>
2023-06-29 14:36:53 +0000

    Commit of task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -32,7 +32,8 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		for (i = 0; *(src + i) != '\0'; i++)
 			*(dest + i) = *(src + i);
-		*(dest + i) = '\0';
+		for (i = _strlen(src); i < n; i++)
+			*(dest + i) = '\0';
 		return (dest);
 	}
 	for (i = 0; i < n; i++)
