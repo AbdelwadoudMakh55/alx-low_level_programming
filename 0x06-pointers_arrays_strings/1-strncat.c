e7f678d2e10ce601f335a8cebf8df9c507e3b00f
AbdelwadoudMakh55 <Abdelwadoud315@gmail.com>
2023-06-29 13:40:50 +0000

    Commit of task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -28,6 +28,13 @@ char *_strncat(char *dest, char *src, int n)
 	int len = _strlen(dest);
 	int i;
 
+	if (n > _strlen(src))
+	{
+		for (i = 0; *(src + i) != '\0'; i++)
+			*(dest + len + i) = *(src + i);
+		*(dest + _strlen(dest)) = '\0';
+		return (dest);
+	}
 	for (i = 0; i < n; i++)
 		*(dest + len + i) = *(src + i);
 	*(dest + _strlen(dest)) = '\0';
