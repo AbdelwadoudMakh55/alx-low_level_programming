b50aa7e054f86abccd30616475a08389d02950d7
AbdelwadoudMakh55 <Abdelwadoud315@gmail.com>
2023-07-03 17:26:33 +0000

    Commit of task 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -15,7 +15,7 @@ void print_diagsums(int *a, int size)
 	for (i = 0; i < size; i++)
 	{
 		sum1 += a[i];
-		sum2 += a[size - i];
+		sum2 += a[size - i - 1];
 		a += size;
 	}
 	printf("%d, %d\n", sum1, sum2);
