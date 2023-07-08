7527ff542eecf707d2e8c760030e5e3f3e33c642
AbdelwadoudMakh55 <Abdelwadoud315@gmail.com>
2023-07-08 17:33:11 +0000

    Commit of task 101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,17 +7,19 @@
 void print_number(int n)
 {
 	int compteur = 0;
-	int power_10 = 1;
+	unsigned int power_10 = 1;
 	int i;
 	int digit = 0;
-	int copy_n = n;
+	unsigned int copy_n = n;
+	unsigned int num = n;
 
 	if (n == 0)
 		_putchar(48);
 	if (n < 0)
 	{
 		_putchar(45);
-		n = -n;
+		num = -n;
+		copy_n = -n;
 	}
 	while (copy_n != 0)
 	{
@@ -26,11 +28,21 @@ void print_number(int n)
 	}
 	for (i = 0; i < compteur - 1; i++)
 		power_10 *= 10;
-	while (n != 0)
+	while (num != 0)
 	{
-		digit = 48 + n / power_10;
-		_putchar(digit);
-		n = n % power_10;
-		power_10 /= 10;
+		if (num == power_10)
+		{
+			_putchar(49);
+			for (i = 1; i < compteur; i++)
+				_putchar(48);
+			num = 0;
+		}
+		else
+		{
+			digit = 48 + num / power_10;
+			_putchar(digit);
+			num = num % power_10;
+			power_10 /= 10;
+		}
 	}
 }
