be513e2057603c26c460ae54b2e6a578b656dc31
AbdelwadoudMakh55 <Abdelwadoud315@gmail.com>
2023-07-09 14:23:23 +0000

    Commit of task 104

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -8,7 +8,7 @@
  */
 void print_buffer(char *b, int size)
 {
-	int i, j;
+	int i, j, k;
 
 	if (size <= 0)
 		printf("\n");
@@ -22,9 +22,9 @@ void print_buffer(char *b, int size)
 			else
 				printf(" %.2x%.2x", b[j], b[j + 1]);
 		}
-		if (j != i + 10)
-			printf("         ");
 		printf(" ");
+		for (k = 0; k < 10 - (size - i); k++)
+			printf(" ");
 		for (j = i; (j < i + 10 && j < size); j++)
 		{
 			if (b[j] <= 126 && b[j] >= 32)
