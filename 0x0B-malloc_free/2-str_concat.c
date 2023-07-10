7df1733213059f5a65a917bf34d3307828b1bcb4
AbdelwadoudMakh55 <Abdelwadoud315@gmail.com>
2023-07-10 14:20:20 +0000

    Commit of task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -54,8 +54,8 @@ char *str_concat(char *s1, char *s2)
 			return (0);
 		for (i = 0; s1[i] != '\0'; i++)
 			conca[i] = s1[i];
-		for (j = 0; s2[j] != '\0'; i++)
-			conca[i] = s2[j];
+		for (j = 0; s2[j] != '\0'; j++)
+			conca[i + j] = s2[j];
 	}
 	return (conca);
 }
