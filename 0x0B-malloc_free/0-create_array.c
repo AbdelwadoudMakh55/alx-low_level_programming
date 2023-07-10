0ea3264c5fcb7e356c02d6642358b3a689c0fbd8
AbdelwadoudMakh55 <Abdelwadoud315@gmail.com>
2023-07-10 12:10:47 +0000

    Commit of task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	array = (char *)malloc(size);
 	if (size == 0)
 		return (0);
-	for (i = 0; i < size; i++)
+	for (i = 1; i < size; i++)
 		array[i] = c;
 	return (array);
 }
