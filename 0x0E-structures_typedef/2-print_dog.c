575fae2e1f5781c4430798fe59dd9a92a67cd54b
AbdelwadoudMakh55 <Abdelwadoud315@gmail.com>
2023-07-17 19:50:42 +0000

    Commit of task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - Prints a struct dog.
+ * @d : Pointer to struct dog.
+ * Return: Void.
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+		printf("Name: %s\nAge: %f\nOwner: %s\n", (*d).name, (*d).age, 
+				(*d).owner);
+}
