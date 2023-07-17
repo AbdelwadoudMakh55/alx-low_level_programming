b8e670e8b3bde2c92a46446380fd5ec593ce8538
AbdelwadoudMakh55 <Abdelwadoud315@gmail.com>
2023-07-17 21:19:50 +0000

    Commit of task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,11 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - Free the memory taken by a dog.
+ * @d : Pointer to dog.
+ * Return: Void (Free memory).
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+}
