9efbef8d0552343eac79ba9319bf2a4c7131b39e
AbdelwadoudMakh55 <Abdelwadoud315@gmail.com>
2023-07-20 13:06:46 +0000

    Commit of task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,7 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(list_nums, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%u", va_arg(list_nums, unsigned int));
+		printf("%d", va_arg(list_nums, int));
 		if (i != n - 1)
 			printf("%s", separator);
 	}
