ba5a0da1f31eb038b2bf5756a2abb2774b7b3409
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-07-27 16:20:56 +0000

    Commit of task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h : list_t list.
+ * Return: Number of nodes (size_t).
+ */
+size_t print_list(const list_t *h)
+{
+	size_t n_nodes = 0;
+	list_t *ptr = (*h).next;
+
+	if ((*h).str == NULL)
+		printf("[0] (nil)\n");
+	else
+		printf("[%u] %s\n", (*h).len, (*h).str);
+	n_nodes++;
+	while (ptr != NULL)
+	{
+		if ((*ptr).str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", (*ptr).len, (*ptr).str);
+		n_nodes++;
+		ptr = (*ptr).next;
+	}
+	return (n_nodes);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+README of 0x12-singly_linked_lists
