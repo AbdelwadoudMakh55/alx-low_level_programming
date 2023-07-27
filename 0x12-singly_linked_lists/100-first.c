d7053277cfd69e7f9ac8cca63ba1e81c290fd981
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-07-27 21:41:43 +0000

    Commit of task 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,8 @@
  * print_before_main - Print a sentence before the main function is executed.
  * Return: Void.
  */
-__attribute__((constructor)) void print_before_main(void)
+__attribute__((constructor))void print_before_main(void)
 {
-	printf("%s", "You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("%s",
+	"You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
