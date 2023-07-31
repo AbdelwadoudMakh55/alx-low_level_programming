6bb81212f3aec341ed8a6bfda22d0beab6d910b3
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-07-31 15:35:04 +0000

    Commit of task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,11 +10,16 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	temp = *head;
-	while (temp != NULL)
-	{
-		temp = (*temp).next;
+	if (*head == NULL)
 		free(*head);
-		*head = temp;
+	else
+	{
+		temp = *head;
+		while (temp != NULL)
+		{
+			temp = (*temp).next;
+			free(*head);
+			*head = temp;
+		}
 	}
 }
