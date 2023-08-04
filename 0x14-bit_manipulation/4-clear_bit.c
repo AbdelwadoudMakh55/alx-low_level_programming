8c02c5ef5a4fdcca6a71952d6e392bca8722a521
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-08-04 00:34:02 +0000

    Commit of task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * clear_bit - Set the bit to 1.
+ * @n : Number.
+ * @index : Index.
+ * Return: 1 or -1.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 31)
+		return (-1);
+	*n &= 1 << index;
+	return (1);
+}
