e5f0fbffb1013097521e8fc8c0def7dcf0ff78ad
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-08-04 01:10:44 +0000

    Commit of task 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,10 +8,9 @@
  * Return: 1 or -1.
  */
 int clear_bit(unsigned long int *n, unsigned int index)
-{
-	
+{	
 	if (index > 31)
 		return (-1);
-	*n &= ((INT_MAX - 1) << index);
+	*n &= (INT_MAX - 1) << index;
 	return (1);
 }
