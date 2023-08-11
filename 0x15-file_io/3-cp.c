5ac7593cdc7db2a2dfbfb57443ad4e27d0618d08
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-08-11 18:32:34 +0000

    Commit of task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,39 +16,24 @@ int main(int argc, char *argv[])
 	ssize_t len;
 
 	if (argc != 3)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
 	file_from = open(argv[1], O_RDONLY);
 	if (argv[1] == NULL || file_from == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR
 	| S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (file_to == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	len = read(file_from, buffer, 1024);
-	while (len != 0)
-	{
-		len = write(file_to, buffer, len);
-		len = read(file_from, buffer, 1024);
-	}
+		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+	while ((len = read(file_from, buffer, 1024)) > 0)
+		if (write(file_to, buffer, len) != len)
+			dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+	if (len == -1)
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	close_source = close(file_from);
 	close_destination = close(file_to);
 	if (close_source == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
+		dprintf(2, "Error: Can't close fd %d\n", file_from), exit(100);
 	if (close_destination == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", file_to), exit(100);
-	}
+		dprintf(2, "Error: Can't close fd %d\n", file_to), exit(100), exit(100);
 	return (0);
 }
