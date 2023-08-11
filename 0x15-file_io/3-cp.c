5628936b1f81b25657da71db6864d5e830534cad
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-08-11 18:13:39 +0000

    Commit of task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,21 +12,20 @@
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, close_source, close_destination;
+	char buffer[3000];
 	ssize_t len;
-	char buffer[1025];
 
 	if (argc != 3)
 	{
-		dprintf(2,"Usage: cp %s %s\n", argv[1], argv[2]);
+		dprintf(2, "Usage: cp %s %s\n", argv[1], argv[2]);
 		exit(97);
 	}
 	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
+	if (argv[1] == NULL || file_from == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	len = read(file_from, buffer, 1024);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR
 	| S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (file_to == -1)
@@ -34,7 +33,12 @@ int main(int argc, char *argv[])
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	len = write(file_to, buffer, len);
+	len = read(file_from, buffer, 1024);
+	while (len != 0)
+	{
+		len = write(file_to, buffer, len);
+		len = read(file_from, buffer, 1024);
+	}
 	close_source = close(file_from);
 	close_destination = close(file_to);
 	if (close_source == -1)
@@ -44,8 +48,7 @@ int main(int argc, char *argv[])
 	}
 	if (close_destination == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", file_to);
-		exit(100);
+		dprintf(2, "Error: Can't close fd %d\n", file_to), exit(100);
 	}
 	return (0);
 }
