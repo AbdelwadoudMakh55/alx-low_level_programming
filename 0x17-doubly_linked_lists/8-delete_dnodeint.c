7b404b22dc9470117321039db479517f15ea752e
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-09-14 22:32:42 +0000

    Commit of task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,22 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * doublylistint_len - returns the number of elements in a dlistint_t list.
- * @h : Pointer to head of dlistint_t list.
- * Return: Number of nodes.
- */
-size_t doublylistint_len(dlistint_t *h)
-{
-	size_t len = 0;
-
-	while (h != NULL)
-	{
-		len++;
-		h = (*h).next;
-	}
-	return (len);
-}
-/**
  * delete_dnodeint_at_index - deletes the node at index index of a dlistint_t
  * linked list.
  * @head : Pointer to Pointer of head of dlistint_t.
@@ -26,40 +10,34 @@ size_t doublylistint_len(dlistint_t *h)
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int i = 0;
-	dlistint_t *current, *prev_n, *next_n;
+	dlistint_t *current, *next_n, *prev_n;
 
-	if (head == NULL || doublylistint_len(*head) == 0
-	|| index >= doublylistint_len(*head) || *head == NULL)
+	if (*head == NULL || head == NULL)
 		return (-1);
 	current = *head;
 	if (index == 0)
 	{
 		next_n = (*current).next;
-		if (next_n == NULL)
+		if (next_n != NULL)
 		{
 			free(current);
-			*head = NULL;
+			(*next_n).prev = NULL;
+			*head = next_n;
 			return (1);
 		}
 		free(current);
-		*head = next_n;
+		*head = NULL;
 		return (1);
 	}
-	while (i != index && current != NULL)
+	while (i < index && current != NULL)
 	{
 		prev_n = current;
 		current = (*current).next;
 		next_n = (*current).next;
 		i++;
 	}
-	if (i != index)
+	if (index > i)
 		return (-1);
-	if (i == index && next_n == NULL)
-	{
-		free(current);
-		(*prev_n).next = NULL;
-		return (1);
-	}
 	free(current);
 	(*prev_n).next = next_n;
 	(*next_n).prev = prev_n;
