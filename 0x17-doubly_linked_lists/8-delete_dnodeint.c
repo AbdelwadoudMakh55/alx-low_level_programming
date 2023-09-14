0abaf3b20abe32e40a069cd34656f2b6f13d6a2f
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-09-14 16:56:16 +0000

    Commit of task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -30,6 +30,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (head == NULL || doublylistint_len(*head) == 0)
 		return (-1);
+	if (index == 0 && *head == NULL)
+		return (1);
 	if (index == 0)
 	{
 		current = (*current).next;
