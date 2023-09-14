034349ecd57e771f549068c6515ef70806aa2507
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-09-14 14:15:45 +0000

    Commit of task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,22 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * doublylistint_len - returns the number of elements in a dlistint_t list.
- * @h : Pointer to head of dlistint_t list.
- * Return: Number of nodes.
- */
-size_t doublylistint_len(dlistint_t *h)
-{
-	size_t len = 0;
-
-	while (h != NULL)
-	{
-		len++;
-		h = (*h).next;
-	}
-	return (len);
-}
-/**
  * insert_dnodeint_at_index - inserts a new node at a given position.
  * @h : Pointer to Pointer to head of dlistint_t list.
  * @idx : Index of node to be inserted.
@@ -32,10 +16,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	if (idx == doublylistint_len(*h))
-		return (add_dnodeint_end(h, n));
-	if (idx > doublylistint_len(*h))
-		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
