622db7879ea3cc0d725f2117c4525a97ca367136
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-10-06 14:28:49 +0000

    Commit of task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -21,7 +21,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (new_pair == NULL)
 		return (0);
 	(*new_pair).key = (char *)key;
-	(*new_pair).value = value;
+	(*new_pair).value = strdup(value);
 	if (l_list[index] == NULL)
 	{
 		(*new_pair).next = NULL;
