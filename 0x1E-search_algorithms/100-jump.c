c26bfa972b7c43974fab6c8bd38796f74869d061
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-01-16 12:26:56 +0000

    Time complexity of jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,13 +13,6 @@ int jump_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
-	if (array[0] == value)
-	{
-		printf("Value found between indexes [0] and [%lu]\n",
-			i + j_block);
-		printf("Value checked array[0] = [%d]\n", array[0]);
-		return (0);
-	}
 	for (; i < size; i += j_block)
 	{
 		if (array[i] >= value)
