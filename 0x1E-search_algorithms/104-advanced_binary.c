330977caffec9d7e2283f1b6b54b72146cd2004e
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-01-17 19:15:42 +0000

    Advanced binary search

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,46 @@
+#include "search_algos.h"
+/**
+ * print_array - Printing array.
+ * @array: Pointer to first element of array.
+ * @l: lowest point of array.
+ * @h: Highest point.
+ * Return: NULL.
+ */
+void print_array(int *array, size_t l, size_t h)
+{
+	printf("Searching in array: ");
+	for (; l <= h; l++)
+	{
+		if (l == h)
+			printf("%d", array[l]);
+		else
+			printf("%d, ", array[l]);
+	}
+	printf("\n");
+}
+/**
+ * advanced_binary - Function for binary search.
+ * @array: Pointer to first element of array.
+ * @size: Size of array.
+ * @value: Value to search for.
+ * Return: index of value in array or -1.
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	size_t l = 0, h = size - 1, mid;
+
+	if (array == NULL)
+		return (-1);
+	while (l <= h)
+	{
+		mid = (h + l) / 2;
+		print_array(array, l, h);
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] > value)
+			h = mid - 1;
+		else
+			l = mid + 1;
+	}
+	return (-1);
+}
