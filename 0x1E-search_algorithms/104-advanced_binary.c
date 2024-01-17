34b4a6d6b13aebd26e01b543713a152ae8cceab4
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-01-17 21:05:01 +0000

    Time complexity

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -19,6 +19,42 @@ void print_array(int *array, size_t l, size_t h)
 	printf("\n");
 }
 /**
+ * recursive_binary - Function for recursive binary search.
+ * @array: Pointer to first element of array.
+ * @value: Value to search for.
+ * @l: Low pointer.
+ * @h: High pointer.
+ * Return: index of value in array or -1.
+ */
+int recursive_binary(int *array, int value, size_t l, size_t h)
+{
+	size_t mid;
+	int f_index, index = -1;
+
+	mid = (h + l) / 2;
+	print_array(array, l, h);
+	if (array[mid] == value)
+	{
+                f_index = (int)mid;
+		h = mid - 1;
+		l = 0;
+		index = recursive_binary(array, value, l, h);
+		if (index == -1)
+			index = f_index;
+	}
+	else if (array[mid] > value)
+	{
+		h = mid - 1;
+		index = recursive_binary(array, value, l, h);
+	}
+	else
+	{
+		l = mid + 1;
+		index = recursive_binary(array, value, l, h);
+	}
+	return (index);
+}
+/**
  * advanced_binary - Function for binary search.
  * @array: Pointer to first element of array.
  * @size: Size of array.
@@ -28,19 +64,28 @@ void print_array(int *array, size_t l, size_t h)
 int advanced_binary(int *array, size_t size, int value)
 {
 	size_t l = 0, h = size - 1, mid;
+	int index = -1, f_index;
 
 	if (array == NULL)
 		return (-1);
-	while (l <= h)
+	mid = (h + l) / 2;
+	if (array[mid] > value)
 	{
-		mid = (h + l) / 2;
-		print_array(array, l, h);
-		if (array[mid] == value)
-			return (mid);
-		else if (array[mid] > value)
-			h = mid - 1;
-		else
-			l = mid + 1;
+		h = mid - 1;
+		index = recursive_binary(array, value, l, h);
 	}
-	return (-1);
+	if (array[mid] < value)
+	{
+		l = mid + 1;
+		index = recursive_binary(array, value, l, h);
+	}
+	if (array[mid] == value)
+	{
+		f_index = (int)mid;
+		h = mid - 1;
+		index = recursive_binary(array, value, l, h);
+		if (index == -1)
+			index = f_index;
+	}
+	return (index);
 }
