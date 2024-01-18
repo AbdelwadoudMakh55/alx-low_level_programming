f6389878526f874fad28744c0c9dcb3f061679ea
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-01-18 12:21:46 +0000

    Fixing jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,11 +13,9 @@ int jump_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
+	printf("Value checked array[0] = [%d]\n", array[0]);
 	if (array[0] == value)
-	{
-		printf("Value checked array[0] = [%d]\n", array[0]);
 		return (0);
-	}
 	while (i < size && array[i] < value)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
